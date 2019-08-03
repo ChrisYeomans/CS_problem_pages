1605873f0dbaca557d2389ec3933fe4779a0b895
ChrisYeomans <chrisj.yeomans@gmail.com>
2019-08-03 13:42:51 +0100

    added Aptfile for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_104550) do
+ActiveRecord::Schema.define(version: 2019_08_03_121536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
