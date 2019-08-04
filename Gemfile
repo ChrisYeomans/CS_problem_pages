850a681015ad2c2fcb690c01e85b42532f1f9cee
ChrisYeomans <chrisj.yeomans@gmail.com>
2019-08-04 19:57:15 +0100

    Fixed gravatar stuff

--- Gemfile
+++ Gemfile
@@ -6,9 +6,11 @@ ruby '2.5.1'
 gem 'redcarpet'
 gem 'rouge'
 
+gem 'gravtastic'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
-# Use postgres as the database for Active Record
+# Use postgresql as the database for Active Record
 gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
+	include Gravtastic
+	gravtastic
+
 	validates :name, presence: true, uniqueness: true, length: { in: 2..25 }	
 	validates :email, presence: true, uniqueness: true, length: { maximum: 100 }
 	validates :password, presence: true, length: { minimum: 6 }, :if => :password
