af384e6cbb5c7648ff07ab22f437796b1ad657ea
ChrisYeomans <chrisj.yeomans@gmail.com>
2019-08-30 14:19:51 +0100

    language test use comment

--- test/system/languages_test.rb
+++ test/system/languages_test.rb
@@ -1,6 +1,63 @@
 require "application_system_test_case"
 
 class LanguagesTest < ApplicationSystemTestCase
+  # All of the following tests are just 
+  # testing each of the supported languages.
+  # If you would like to add a test for a new
+  # language its not very complex.
+  # Firstly the first 2 blocks of code are always
+  # the same:
+  #
+  #   # login as admin
+  #   visit "/login"
+  #   fill_in "Name", with: "admin"
+  #   fill_in "Password", with: "123456"
+  #   find('#submit-id-submit', :visible => true).click
+  #
+  #   # create problem
+  #   visit "/dashboard/create_problem"
+  #   fill_in "Title", with: "test #01"
+  #   fill_in "Problem Statement", with: "test #01 body"
+  #   fill_in "Test Cases", with: "***\nhello world"
+  #   fill_in "Max CPU Time", with: "2"
+  #   fill_in "Max CPU Memory", with: "512m"
+  #   fill_in "Score", with: "100"
+  #   find("#problem_is_current").set(false)
+  #   find("#problem_is_hidden").set(false)
+  #   click_on "Submit"
+  #
+  #   # make submissions
+  #   click_on "Make Submission"
+  #
+  # The above code does at it says, it
+  # logis in as an admin user and makes a
+  # "hello world" problem.
+  # The next block of code is as follows:
+  #   attach_file("submission[upload]", Rails.root + "test/system/test_files/language_test.extension")
+  #   select("language", from: "submission_language")
+  #   click_on "Submit"
+  # This block of code needs to be modified by
+  # filling in the language name where it says
+  # "language" and by filling in the language
+  # extension where is says "extension".
+  # Finally the last bit of code is just to
+  # wait for the submission results and check 
+  # for the result:
+  #
+  #   # sleep to wait for ajax
+  #   sleep 1
+  #
+  #   assert_no_text "Verdict: 0"
+  #   assert_text "Verdict: 1"
+  # Now hereafter you need to make a file in
+  # the new language that is called:
+  #    language_test.extension
+  # replacing "language" and "extension"
+  # appropriately.
+  # The file must output "hello world" and not
+  # take any input.
+
+
   test "python3" do
     # login as admin
     visit "/login"
